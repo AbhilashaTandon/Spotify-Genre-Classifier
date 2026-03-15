c0e5618218868d54e7f2ca4be49e4d140c2ce6b4
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-03-15 14:10:54 -0400

    Increased max playlists per search

--- src/main.py
+++ src/main.py
@@ -15,21 +15,23 @@ search_requests: list[str] = []
 
 
 def get_playlist_ids(api_token: str):
-    with open('../data/playlist_ids.txt', mode='a') as output_file:
+    with open("../data/playlist_ids.txt", mode="r+") as output_file:
         for search_request in search_requests:
-            extract_playlists(search_request, api_token, output_file)
+            extract_playlists(
+                search_request, api_token, output_file, max_playlists=2000
+            )
 
 
 def main():
 
-    with open('secrets.json', mode='r') as secrets_file:
+    with open("secrets.json", mode="r") as secrets_file:
         secrets = json.load(secrets_file)
 
     api_token = request_access_token(secrets)
 
     print(api_token)
 
-    with open('data/output.json', mode='r') as output_file:
+    with open("data/output.json", mode="r") as output_file:
         output = output_file.read()
         # removes comma and whitespace at end of file
         # by wrapping file in curly braces we make it a single json obj
@@ -37,10 +39,10 @@ def main():
         output_json = json.loads(output)
         past_ids = set(output_json.keys())
 
-        with open('data/playlist_ids.txt', mode='r') as input_file:
+        with open("data/playlist_ids.txt", mode="r") as input_file:
             ids = set(input_file.read().splitlines())
 
-            num_playlists = len(ids)
+            # num_playlists = len(ids)
 
             # remove playlists we've already extracted
             ids = list(ids - past_ids)
@@ -49,31 +51,34 @@ def main():
 
             to_be_extracted = len(ids)
 
-    with open('data/output.json', mode='w') as output_file:
+    with open("data/output.json", mode="w") as output_file:
         with alive_bar(offset + to_be_extracted) as bar:
             bar(offset, skipped=True)
             for idx, playlist_id in enumerate(ids):
-                api_req = 'https://api.spotify.com/v1/playlists/' + \
-                    playlist_id + '/tracks?limit=50&offset=0'
+                api_req = (
+                    "https://api.spotify.com/v1/playlists/"
+                    + playlist_id
+                    + "/tracks?limit=50&offset=0"
+                )
                 try:
                     tracks = extract_tracks(api_req, api_token)
                     output_json[playlist_id] = tracks
                     bar()
                 except FatalError as e:
                     output_file.write(json.dumps(output_json))
-                    with open('log.txt', mode='a') as log_file:
+                    with open("log.txt", mode="a") as log_file:
                         log_file.write(
-                            f"{e.message}. stopped at playlist {idx + offset}\n")
+                            f"{e.message}. stopped at playlist {idx + offset}\n"
+                        )
                     return
                 except NonFatalError as e:
-                    with open('log.txt', mode='a') as log_file:
-                        log_file.write(
-                            f"playlist {idx + offset} {e}\n")
+                    with open("log.txt", mode="a") as log_file:
+                        log_file.write(f"playlist {idx + offset} {e}\n")
                     bar()
                     continue
 
         output_file.write(json.dumps(output_json))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
